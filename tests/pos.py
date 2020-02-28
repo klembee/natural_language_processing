8953bf1d89d532b17bee470406c856d7ffceaaf1
Clement <clement.bisaillon@hotmail.com>
2020-02-27 19:14:08 -0500

    part of speech example

---
+++ tests/pos.py
@@ -0,0 +1,30 @@
+import nltk
+
+text = "Japanese is such a great country. It has suchis, castles and gardens !".split()
+print("Text: {}".format(text))
+tokens_tag = nltk.pos_tag(text)
+print("Tokens: {}".format(tokens_tag))
+
+patterns = """superimportant:{<NN.?>*<VBD.?>*<JJ.?>*<CC>?}"""
+chunker = nltk.RegexpParser(patterns)
+print("Chunks: {}".format(chunker.parse(tokens_tag)))
+
+# Graphing the chunks
+
+print("\nGraphing chunks")
+
+text = "Rice vinegar recipe: ¼ cup seasoned rice vinegar, 1 small garlic clove and 1 cup of rice !"
+tokens = nltk.word_tokenize(text)
+print("Text: {}".format(text))
+print("Tokens: {}".format(tokens))
+
+tag = nltk.pos_tag(tokens)
+print("Tags: {}".format(tag))
+
+grammar = "NP: {<DT>?<JJ>*<NN>}"
+cp = nltk.RegexpParser(grammar)
+result = cp.parse(tag)
+print("Result: {}".format(result))
+result.draw()
+
+
